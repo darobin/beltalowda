6fce8c8feb28711c48102631437aee61d7167d22
Robin Berjon <robin.berjon@nytimes.com>
2023-01-19 18:29:22 -0500

    identity demo

--- pluggable-identity/pluggable.js
+++ pluggable-identity/pluggable.js
@@ -20,31 +20,36 @@ export class CatSocietyLoginButton extends LitElement {
     sl-button::part(base):hover {
       background: hotpink;
     }
+    img {
+      border-radius: 50%;
+      border: 2px solid hotpink;
+    }
   `;
   static properties = {
     state: {
+      attribute: false,
+    },
+    identity: {
       attribute: false,
     },
   };
   constructor () {
     super();
     this.state = state.get().state;
+    this.identity = null;
     state.sub((data) => {
       this.state = data.state;
+      this.identity = data.identity;
     });
   }
   handleRequestIdentity () {
-    console.warn(`requesting`);
     requestLogin();
-    console.warn(`data`, state.get());
   }
   render () {
     if (this.state === 'logged-in') {
       return html`
-        <sl-button size="small" outline>
-          <sl-icon slot="prefix" name="person-circle"></sl-icon>
-          SOME PERSON
-        </sl-button>`
+        <img src=${`../img/${this.identity.img}`} width="32" alt=${`${this.identity.name} picture`}>
+      `
       ;
     }
     const loading = (this.state === 'requesting-login');
@@ -58,3 +63,144 @@ export class CatSocietyLoginButton extends LitElement {
 }
 
 customElements.define('cat-login', CatSocietyLoginButton);
+
+export class BeltalowdaIdentity extends LitElement {
+  static styles = css`
+    :host {
+      --arrow-color: #000;
+    }
+    sl-popup > div {
+      background: #fff;
+      border: solid var(--border-width) var(--border-color);
+      border-radius: var(--border-radius);
+    }
+    sl-popup > .requesting {
+      width: 280px;
+    }
+    sl-popup > .logged-in {
+      width: 48px;
+      height: 48px;
+      filter: sepia(1);
+      transition: opacity width 0.5s;
+    }
+    sl-popup > .logged-in img { display: revert; }
+    sl-popup > .logged-in sl-icon-button { display: none; }
+    sl-popup > .logged-in:hover img { display: none; }
+    sl-popup > .logged-in:hover sl-icon-button { display: revert; }
+    sl-option > img {
+      border-radius: 50%;
+    }
+    .actions {
+      text-align: right;
+      padding: 0.5rem 1rem;
+    }
+    sl-icon-button::part(base):hover {
+      color: inherit;
+    }
+  `;
+  static properties = {
+    anchor: {},
+    state: {
+      attribute: false,
+    },
+    identity: {
+      attribute: false,
+    },
+    selectedIdentity: {
+      attribute: false,
+    },
+  };
+  constructor () {
+    super();
+    this.state = state.get().state;
+    this.identity = null;
+    state.sub((data) => {
+      this.state = data.state;
+      this.identity = data.identity;
+    });
+    this.identities = [
+      { id: 'unicorn', img: 'unicorn.jpg', name: 'Unicorn' },
+      { id: 'robin', img: 'robin.jpg', name: 'Robin' },
+      { id: 'wtf', img: 'wtf-cat.jpg', name: 'Mew Mew Mew' },
+    ];
+    this.selectedIdentity = null;
+  }
+  handlePickIdentity (ev) {
+    const id = ev.originalTarget.value;
+    this.selectedIdentity = this.identities.find(p => p.id === id);
+  }
+  handleAccept () {
+    chooseIdentity(this.selectedIdentity);
+  }
+  handleAbort () {
+    this.selectedIdentity = null;
+    logout();
+  }
+  handleLogout () {
+    this.selectedIdentity = null;
+    logout();
+  }
+  render () {
+    const active = (this.state !== 'logged-out');
+    if (!this.anchor) return;
+    // not sure why but we have to resolve it ourselves or it won't work
+    const anchor = this.getRootNode().getElementById(this.anchor);
+    let content;
+    if (this.state === 'requesting-login') {
+      content = html`
+        <div class="requesting">
+          <sl-select placeholder="Pick login identity…" @sl-change=${this.handlePickIdentity}>
+            <sl-icon name="person-circle" slot="prefix"></sl-icon>
+            ${
+              this.identities.map(({ id, img, name }) => html`<sl-option value=${id}><img src=${`../img/${img}`} width="32" alt=${`${name} picture`} slot="prefix">${name}</sl-option>`)
+            }
+          </sl-select>
+          <div class="actions">
+            <sl-button size="small" @click=${this.handleAbort}>Cancel</sl-button>
+            <sl-button size="small" @click=${this.handleAccept} disabled=${this.selectedIdentity ? nothing : true} variant="primary">Login</sl-button>
+          </div>
+        </div>
+      `;
+    }
+    else if (this.state === 'logged-in') {
+      content = html`
+        <div class="logged-in">
+          <img src=${`../img/${this.identity.img}`} width="48" alt=${`${this.identity.name} picture`}>
+          <sl-icon-button name="box-arrow-right" label="Logout" id="logout-icon" style="font-size: 2rem; text-align: center;" @click=${this.handleLogout}></sl-icon-button>
+        </div>
+      `;
+    }
+    return html`
+      <sl-popup
+        active=${active || nothing}
+        placement="right-start"
+        .anchor=${anchor}
+        skidding="53"
+        arrow
+        arrow-placement="start"
+        arrow-padding="5"
+        distance="5"
+      >
+        ${content}
+      </sl-popup>`
+    ;
+}
+}
+
+customElements.define('blt-identity', BeltalowdaIdentity);
+
+/*
+LOGGED OUT
+- [x] button shows "login"
+- [x] no ID float
+
+REQUESTING
+- [x] button loading
+- [ ] float with DD, arrow, some distance
+- [ ] can cancel
+
+LOGGED IN
+- [ ] no button just face
+- [ ] float with ID, logout button, no arrow, no distance, slightly transparent
+
+*/
