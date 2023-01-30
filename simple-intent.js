23d96e17f52ac18c65f3ac6cbc062f0b6785ecc9
Robin Berjon <robin.berjon@nytimes.com>
2023-01-30 18:06:08 -0500

    h thing with docs

--- simple-intent.js
+++ simple-intent.js
@@ -40,3 +40,39 @@ export class BeltalowdaInstallIntentIcon extends LitElement {
 }
 
 customElements.define('blt-install-intent', BeltalowdaInstallIntentIcon);
+
+export class BeltalowdaSimpleIntentState extends LitElement {
+  static styles = css`
+    p {
+      margin: 0;
+      padding: 1rem;
+      border: 1px solid var(--sl-color-primary-700);
+      background: var(--sl-color-primary-100);
+      font-size: 0.8rem;
+      max-width: 300px;
+    }
+  `;
+  static properties = {
+    isInstalled: {
+      attribute: false,
+    },
+  };
+  constructor () {
+    super();
+    this.isInstalled = !!state.get().intents?.length;
+    state.sub((data) => {
+      this.isInstalled = !!data.intents?.length;
+    });
+  }
+  render () {
+    if (this.isInstalled) return html`<p>
+      Now that the intent is installed, it will respond when the user seeks to <strong>pick an image/*</strong>.
+      You can remove the intent by clicking again.
+    </p>`;
+    return html`<p>
+      Install the intent by clicking on the top-right icon.
+    </p>`;
+  }
+}
+
+customElements.define('blt-simple-intent-state', BeltalowdaSimpleIntentState);
